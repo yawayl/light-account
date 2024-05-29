3f5519a689839d77899ccec2254069e696efe4e0
0xfourzerofour <joshpauline@gmail.com>
2024-05-29 14:31:37 -0400

    feat(temporal): create repeatable deploy script

--- script/Deploy_LightAccountFactory.s.sol
+++ script/Deploy_LightAccountFactory.s.sol
@@ -2,32 +2,36 @@
 pragma solidity ^0.8.19;
 
 import "forge-std/Script.sol";
-
+import "forge-std/StdJson.sol";
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
-
 import {LightAccountFactory} from "../src/LightAccountFactory.sol";
+import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 
 contract Deploy_LightAccountFactory is Script {
-    // Load entrypoint from env
-    address public entryPointAddr = vm.envAddress("ENTRYPOINT");
-    IEntryPoint public entryPoint = IEntryPoint(payable(entryPointAddr));
+    using stdJson for string;
 
-    // Load factory owner from env
-    address public owner = vm.envAddress("OWNER");
+    IEntryPoint entryPoint;
+    address entryPointAddr;
+    address expectedAddress;
+    address owner;
+    uint256 salt;
 
     error InitCodeHashMismatch(bytes32 initCodeHash);
     error DeployedAddressMismatch(address deployed);
 
+    function readInputsFromPath() internal {
+        string memory json = vm.readFile("input.json");
+        entryPointAddr = json.readAddress("$.entryPoint");
+        entryPoint = IEntryPoint(payable(entryPointAddr));
+        expectedAddress = json.readAddress("$.address");
+        owner = json.readAddress("$.owner");
+        salt = json.readUint("$.salt");
+    }
+
+
     function run() public {
+        readInputsFromPath();
         vm.startBroadcast();
-
-        // Init code hash check
-        bytes32 initCodeHash =
-            keccak256(abi.encodePacked(type(LightAccountFactory).creationCode, abi.encode(owner, entryPoint)));
-
-        if (initCodeHash != 0xfad339962af095db6ac3163c8504f102c28ae099db994101fbbca18ad0e3005c) {
-            revert InitCodeHashMismatch(initCodeHash);
-        }
 
         console.log("********************************");
         console.log("******** Deploy Inputs *********");
@@ -39,17 +43,9 @@ contract Deploy_LightAccountFactory is Script {
         console.log("******** Deploying.... *********");
         console.log("********************************");
 
-        LightAccountFactory factory = new LightAccountFactory{
-            salt: 0x00000000000000000000000000000000000000005f1ffd9d31306e056bcc959b
-        }(owner, entryPoint);
+        LightAccountFactory factory = deployImpl(bytes32(salt), expectedAddress, owner, entryPoint);
 
-        // Deployed address check
-        if (address(factory) != 0x0000000000400CdFef5E2714E63d8040b700BC24) {
-            revert DeployedAddressMismatch(address(factory));
-        }
-
-        _addStakeForFactory(address(factory));
-
+        // _addStakeForFactory(address(factory));
         console.log("LightAccountFactory:", address(factory));
         console.log("LightAccount:", address(factory.ACCOUNT_IMPLEMENTATION()));
         console.log();
@@ -57,16 +53,34 @@ contract Deploy_LightAccountFactory is Script {
         vm.stopBroadcast();
     }
 
-    function _addStakeForFactory(address factoryAddr) internal {
-        uint32 unstakeDelaySec = uint32(vm.envOr("UNSTAKE_DELAY_SEC", uint32(86400)));
-        uint256 requiredStakeAmount = vm.envUint("REQUIRED_STAKE_AMOUNT");
-        uint256 currentStakedAmount = entryPoint.getDepositInfo(factoryAddr).stake;
-        uint256 stakeAmount = requiredStakeAmount - currentStakedAmount;
-        LightAccountFactory(payable(factoryAddr)).addStake{value: stakeAmount}(unstakeDelaySec, stakeAmount);
-        console.log("******** Add Stake Verify *********");
-        console.log("Staked factory: ", factoryAddr);
-        console.log("Stake amount: ", entryPoint.getDepositInfo(factoryAddr).stake);
-        console.log("Unstake delay: ", entryPoint.getDepositInfo(factoryAddr).unstakeDelaySec);
-        console.log("******** Stake Verify Done *********");
+
+    function deployImpl(bytes32 saltBytes, address expected, address ownerAddr, IEntryPoint ep) private returns (LightAccountFactory) {
+        address addr = Create2.computeAddress(
+            saltBytes, keccak256(abi.encodePacked(type(LightAccountFactory).creationCode, abi.encode(ownerAddr, ep))), CREATE2_FACTORY
+        );
+        console.logAddress(addr);
+        require(addr == expected, "Expected address is not the same as computed for impl");
+        if (addr.code.length > 0) {
+            console.log("VerifyingPaymaster impl already deployed. Skipping");
+            return LightAccountFactory(payable(addr));
+        }
+
+        LightAccountFactory impl = new LightAccountFactory{salt: saltBytes}(ownerAddr, ep);
+        require(address(impl) == addr, "Impl address did not match predicted");
+        return impl;
     }
+
+
+    // function _addStakeForFactory(address factoryAddr) internal {
+    //     uint32 unstakeDelaySec = uint32(vm.envOr("UNSTAKE_DELAY_SEC", uint32(86400)));
+    //     uint256 requiredStakeAmount = vm.envUint("REQUIRED_STAKE_AMOUNT");
+    //     uint256 currentStakedAmount = entryPoint.getDepositInfo(factoryAddr).stake;
+    //     uint256 stakeAmount = requiredStakeAmount - currentStakedAmount;
+    //     LightAccountFactory(payable(factoryAddr)).addStake{value: stakeAmount}(unstakeDelaySec, stakeAmount);
+    //     console.log("******** Add Stake Verify *********");
+    //     console.log("Staked factory: ", factoryAddr);
+    //     console.log("Stake amount: ", entryPoint.getDepositInfo(factoryAddr).stake);
+    //     console.log("Unstake delay: ", entryPoint.getDepositInfo(factoryAddr).unstakeDelaySec);
+    //     console.log("******** Stake Verify Done *********");
+    // }
 }
