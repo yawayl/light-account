a22024f90d729d005ad36691c93f15e5c1f16aa3
0xfourzerofour <joshpauline@gmail.com>
2024-06-21 10:36:03 -0400

    fix(contracts): new input file

--- script/Deploy_LightAccountFactory.s.sol
+++ script/Deploy_LightAccountFactory.s.sol
@@ -2,51 +2,48 @@
 pragma solidity ^0.8.19;
 
 import "forge-std/Script.sol";
-
+import "forge-std/StdJson.sol";
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
-
 import {LightAccountFactory} from "../src/LightAccountFactory.sol";
+import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 
-// @notice Deploys LightAccountFactory to the address `0x00004EC70002a32400f8ae005A26081065620D20`
+// Deploys LightAccount to 0x00004EC70002a32400f8ae005A26081065620D20
 // @dev Note: Script uses EntryPoint at address 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
 // @dev To run: `forge script script/Deploy_LightAccountFactory.s.sol:Deploy_LightAccountFactory --broadcast --rpc-url ${RPC_URL} --verify -vvvv`
 contract Deploy_LightAccountFactory is Script {
+    using stdJson for string;
+
     error InitCodeHashMismatch(bytes32 initCodeHash);
     error DeployedAddressMismatch(address deployed);
 
+    address entryPoint;
+    address expectedAddress;
+    uint256 salt;
+
+    function readInputsFromPath() internal {
+        string memory json = vm.readFile("input.json");
+        entryPoint = json.readAddress("$.entryPoint");
+        expectedAddress = json.readAddress("$.address");
+        salt = json.readUint("$.salt");
+    }
+
     function run() public {
+
+        readInputsFromPath();
         vm.startBroadcast();
 
-        // Using entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
-        // Correct as of Jan 10 2024, from https://docs.alchemy.com/reference/eth-supportedentrypoints
-        IEntryPoint entryPoint = IEntryPoint(payable(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789));
-
-        // Init code hash check
-        bytes32 initCodeHash = keccak256(
-            abi.encodePacked(type(LightAccountFactory).creationCode, bytes32(uint256(uint160(address(entryPoint)))))
-        );
-
-        if (initCodeHash != 0x23fb754854a6aa03057b1bae5d971963d92e534dc714fa59fff6c08a3617ba3e) {
-            revert InitCodeHashMismatch(initCodeHash);
-        }
+        IEntryPoint entryPointContract = IEntryPoint(payable(entryPoint));
 
         console.log("********************************");
         console.log("******** Deploy Inputs *********");
         console.log("********************************");
         console.log("Entrypoint Address is:");
-        console.logAddress(address(entryPoint));
+        console.logAddress(address(entryPointContract));
         console.log("********************************");
         console.log("******** Deploy ...... *********");
         console.log("********************************");
 
-        LightAccountFactory factory = new LightAccountFactory{
-            salt: 0x4e59b44847b379578588920ca78fbf26c0b4956c5528f3e2f146000008fabf77
-        }(entryPoint);
-
-        // Deployed address check
-        if (address(factory) != 0x00004EC70002a32400f8ae005A26081065620D20) {
-            revert DeployedAddressMismatch(address(factory));
-        }
+        LightAccountFactory factory = deployImpl(bytes32(salt), expectedAddress, entryPointContract);
 
         console.log("LightAccountFactory address:");
         console.logAddress(address(factory));
@@ -55,4 +52,21 @@ contract Deploy_LightAccountFactory is Script {
         console.logAddress(address(factory.accountImplementation()));
         vm.stopBroadcast();
     }
+
+    function deployImpl(bytes32 saltBytes, address expected, IEntryPoint ep) private returns (LightAccountFactory) {
+        address addr = Create2.computeAddress(
+            saltBytes, keccak256(abi.encodePacked(type(LightAccountFactory).creationCode, abi.encode(ep))), CREATE2_FACTORY
+        );
+        console.logAddress(addr);
+        require(addr == expected, "Expected address is not the same as computed for impl");
+        if (addr.code.length > 0) {
+            console.log("VerifyingPaymaster impl already deployed. Skipping");
+            return LightAccountFactory(payable(addr));
+        }
+
+        LightAccountFactory impl = new LightAccountFactory{salt: saltBytes}(ep);
+        require(address(impl) == addr, "Impl address did not match predicted");
+        return impl;
+    }
+
 }
